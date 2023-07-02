eeb3fd31abacaa1a67ff242be5b61791ae4dfec8
ismaelnt <ismaelnt97@gmail.com>
2023-07-02 08:41:01 -0300

    refactor: refac with seed work

---
+++ src/TvPlus.Catalog.Domain/SeedWork/AggregateRoot.cs
@@ -0,0 +1,6 @@
+﻿namespace TvPlus.Catalog.Domain.SeedWork;
+
+public abstract class AggregateRoot : Entity
+{
+    protected AggregateRoot() : base() { }
+}
---
+++ src/TvPlus.Catalog.Domain/SeedWork/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace TvPlus.Catalog.Domain.SeedWork;
+
+public abstract class Entity
+{
+    public Guid Id { get; protected set; }
+
+    protected Entity() => Id = Guid.NewGuid();
+}
