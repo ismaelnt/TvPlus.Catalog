4b5c01c9530dfabf55100309f4631ce598757504
ismaelnt <ismaelnt97@gmail.com>
2023-07-04 04:41:12 -0300

    test: add domain validation

---
+++ tests/TvPlus.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using FluentAssertions;
+using TvPlus.Catalog.Domain.Exceptions;
+using TvPlus.Catalog.Domain.Validation;
+
+namespace TvPlus.Catalog.UnitTests.Domain.Validation;
+
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        string value = Faker.Commerce.ProductName();
+        Action action = () => DomainValidation.NotNull(value, "value");
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThrowWhenNull()
+    {
+        string value = null;
+        Action action = () => DomainValidation.NotNull(value, "FieldName");
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+}
